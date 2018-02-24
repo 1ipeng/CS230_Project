cf516ace8bdc20d06135ba8d4272371c377573cd
Peng Li <lipengshopping@gmail.com>
2018-02-24 14:32:17 -0800

    add vectorization

--- build_dataset.py
+++ build_dataset.py
@@ -1,36 +1,38 @@
 import cPickle
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 from skimage import color
 import sys
+import os
 
 SIZE = 100
 small = (sys.argv[1] == "small")
 
-DATA_TRAIN = ["/data/cifar-10-batches-py/data_batch_1", 
-			  "/data/cifar-10-batches-py/data_batch_2",
-			  "/data/cifar-10-batches-py/data_batch_3",
-			  "/data/cifar-10-batches-py/data_batch_4",
-			  "/data/cifar-10-batches-py/data_batch_5"]
-DATA_TEST = ["/data/cifar-10-batches-py/test_batch"]
+DATA_TRAIN = ["data/cifar-10-batches-py/data_batch_1", 
+			  "data/cifar-10-batches-py/data_batch_2",
+			  "data/cifar-10-batches-py/data_batch_3",
+			  "data/cifar-10-batches-py/data_batch_4",
+			  "data/cifar-10-batches-py/data_batch_5"]
+DATA_TEST = ["data/cifar-10-batches-py/test_batch"]
+DATA_BINS = "data/bins_529.npy"
 
 if small:
-	DIR_TRAIN = "/data/lab_result/" + str(SIZE) + "_train_lab/"
-	DIR_TEST = "/data/lab_result/" + str(SIZE) + "_test_lab/"
+	DIR_TRAIN = "data/lab_result/" + str(SIZE) + "_train_lab/"
+	DIR_TEST = "data/lab_result/" + str(SIZE) + "_test_lab/"
 else:
-	DIR_TRAIN = "/data/lab_result/train_lab/"
-	DIR_TEST = "/data/lab_result/test_lab"
+	DIR_TRAIN = "data/lab_result/train_lab/"
+	DIR_TEST = "data/lab_result/test_lab/"
 
 def unpickle(file):
     with open(file, 'rb') as f:
         dict = cPickle.load(f)
     return dict
 
-def parseImages(filenames, directory):
+def parseImages(filenames, directory, bin_dict, bin_size):
 	channel_L = []
 	channel_ab = []
 	labels = []
+
 	for file in filenames:
 		batch = unpickle(file)
 		raw_images = batch["data"]
@@ -51,45 +53,24 @@ def parseImages(filenames, directory):
 			labels.append(batch_labels[i])
 
 	labels = np.array(labels).reshape(len(labels), -1)
+	channel_ab = np.array(channel_ab)
+	
+	ab2bins = np.vectorize(lambda a, b: bin_dict.index([a // bin_size * bin_size, b // bin_size * bin_size]))
+	bins = ab2bins(channel_ab[:, :, :, 0], channel_ab[:, :, :, 1])
+	bins = bins.reshape(bins.shape[0], bins.shape[1], bins.shape[2], 1)
 
-	return channel_L, channel_ab, labels
-
-def ab2bins(ab):
-	bin_dict = {}
-	ab_dict = {}
-	count = 0
-	bin_size=10
-	for a in np.arange(-110, 120, bin_size):
-		for b in np.arange(-110, 120, bin_size):
-			bin_dict[(a,b)] = count
-			ab_dict[count] = [a, b]
-			count += 1
-	ab = np.array(ab)		
-	m, s1, s2, c = ab.shape
-	bins = np.zeros((m, s1, s2, 1)).astype(int)
-	for i in range(m):
-		for j in range(s1):
-			for k in range(s2):
-				a, b = ab[i, j, k, :] // bin_size * bin_size
-				bins[i, j, k, 0] = bin_dict[(a, b)]
-	return bins
-
-train_L, train_ab, train_labels = parseImages(DATA_TRAIN, DIR_TRAIN)
-test_L, test_ab, test_labels = parseImages(DATA_TEST, DIR_TEST)
-train_bins = ab2bins(train_ab)
-test_bins = ab2bins(test_ab)
-
-
-
-np.save(DIR_TRAIN + "L", train_L)
-np.save(DIR_TRAIN + "ab", train_ab)
-np.save(DIR_TRAIN + "labels", train_labels)
-np.save(DIR_TRAIN + "bins", train_bins)
-np.save(DIR_TEST + "L", test_L)
-np.save(DIR_TEST + "ab", test_ab)
-np.save(DIR_TEST + "labels", test_labels)
-np.save(DIR_TEST + "bins", test_bins)
-
+	if not(os.path.exists(directory)):
+		os.makedirs(directory)
+	np.save(directory + "L", channel_L)
+	np.save(directory + "ab", channel_ab)
+	np.save(directory + "labels", labels)
+	np.save(directory + "bins", bins)
+	return channel_L, channel_ab, labels, bins
+	
+bin_size = 10
+bin_dict = np.load(DATA_BINS).tolist()
+train_L, train_ab, train_labels, train_bins = parseImages(DATA_TRAIN, DIR_TRAIN, bin_dict, bin_size)
+test_L, test_ab, test_labels, test_bins = parseImages(DATA_TEST, DIR_TEST, bin_dict, bin_size)
 print("Done building dataset")
 
 '''
@@ -112,6 +93,3 @@ else:
 	assert(np.load(DIR_TEST + "labels.npy").shape == (10000, 1))
 	assert(np.load(DIR_TEST + "bins.npy").shape == (10000,32, 32, 1))
 '''
-
-
-
